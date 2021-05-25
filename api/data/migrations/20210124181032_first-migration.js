738d5436cf566e3eb9ee44711db1c57d84087888
Chance Swartz <71352443+chance10113@users.noreply.github.com>
2021-05-25 15:40:03 -0500

    Setting up

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -2,9 +2,9 @@ exports.up = async (knex) => {
   await knex.schema
     .createTable('users', (users) => {
       users.increments('user_id')
-      users.string('user_username', 200).notNullable()
+      users.string('user_username', 200).notNullable().unique()
       users.string('user_password', 200).notNullable()
-      users.string('user_email', 320).notNullable()
+      users.string('user_phone', 320).notNullable().unique()
       users.timestamps(false, true)
     })
 }
