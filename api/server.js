9a4bf54fcedd073dbb485329d7c19e97b6f21101
Chance Swartz <71352443+chance10113@users.noreply.github.com>
2021-05-25 22:19:59 -0500

    Finally spinning up

--- api/server.js
+++ api/server.js
@@ -1,10 +1,22 @@
-const express = require('express')
-const helmet = require('helmet')
-const cors = require('cors')
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
 
-const server = express()
-server.use(express.json())
-server.use(helmet())
-server.use(cors())
+const server = express();
+server.use(express.json());
+server.use(helmet());
+server.use(cors());
 
-module.exports = server
+const authRouter = require("./auth/auth-router");
+const usersRouter = require("./users/users-router");
+
+const restricted = require("./auth/auth-restricted");
+
+server.use("/api/auth", authRouter);
+server.use("/api/users", restricted, usersRouter);
+
+server.get("/", (req, res) => {
+  res.json({ Hello: "Everyone!" });
+});
+
+module.exports = server;
